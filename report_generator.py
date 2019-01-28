962bccb40ad7a7610dc000a25920ccffe6dea0ef
LeeKLTW <LeeKLTW@gmail.com>
2019-01-28 14:56:20 +0800

    update generate_reorder_report()

--- report_generator.py
+++ report_generator.py
@@ -37,4 +37,31 @@ def generate_inventory_report():
 
 
 def generate_reorder_report():
-    pass
+    product_names = {}
+    desired_numbers = {}
+
+    for product_code, name, desired_number in data_storage.products():
+        product_names[product_code] = name
+        desired_number[product_code] = desired_number
+
+    num_in_inventory = {}
+    for product_code, location_code in data_storage.items():
+        if product_code in num_in_inventory:
+            num_in_inventory += 1
+        else:
+            num_in_inventory[product_code] = 1
+
+    report = []
+    report.append("RE-ORDER REPORT")
+    report.append("")
+
+    for product_code in sorted(product_names.keys()):
+        desired_number = desired_numbers[product_code]
+        current_number = num_in_inventory.get(product_code, 0)
+        if current_number < desired_number:
+            product_name = product_names[product_code]
+            num_to_reorder = desired_number - current_number
+            report.append(f" Re-order {num_to_reorder} of {product_code} - {product_name}")
+
+    report.append("")
+    return report
